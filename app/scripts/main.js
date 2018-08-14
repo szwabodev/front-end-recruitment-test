837bec24a20d6f0529eb7411ebaa6005643f07d4
unknown <rafal.szwabo@gmail.com>
2018-08-14 12:44:18 +0200

    Task 3: added validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -82,4 +82,97 @@
     // Insert a new bacon section by appending it to the parent of all bacons
     document.getElementById('overview').appendChild(baconCopy);
   });
+
+  // Adds dashes every 4 digits to credit card input
+  const dashAdder = (e) => {
+    e.target.value = e.target.value.replace(/-/g, '');
+    e.target.value = e.target.value.replace(/(.{4})/g, '$1-');
+    e.target.value = e.target.value.replace(/-$/, '');
+  };
+  document.getElementById('credit').addEventListener('keypress', dashAdder);
+  document.getElementById('credit').addEventListener('change', dashAdder);
+
+  // Handles submit button click
+  document.getElementById('complete_button').addEventListener('click', () => {
+    // key = DOM element ID, reg = expression to check on the input, msg = message to display on error
+    const validators = {
+      fname: {
+        reg: /^[a-z]+$/gi,
+        msg: 'The first name can only contain letters.',
+      },
+      lname: {
+        reg: /^[a-z]+$/gi,
+        msg: 'The last name can only contain letters.',
+      },
+      email: {
+        reg: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        msg: 'The provided email is invalid.',
+      },
+      postal: {
+        reg: /^\d{2}-?\d{3}$/,
+        msg: 'The postal code must be in XX-XXX or XXXXX numeric format.',
+      },
+      phone: {
+        reg: /^\(?\+?\d{1,3}\)?(\s|-)?\d{2,4}(\s|-)?\d{2,3}(\s|-)?\d{2,3}$/,
+        msg: 'Invalid phone number provided.',
+      },
+      credit: {
+        reg: /^\d{4}-\d{4}-\d{4}-\d{4}$/,
+        msg: 'Invalid credit card number!',
+      },
+      credit_code: {
+        reg: /^\d{3}$/,
+        msg: 'Invalid card security code.',
+      },
+      exp_date: {
+        reg: /^(\d{1,2})\/(\d{2})$/,
+        msg: 'Invalid expiration date. The correct format is: MM/YY',
+      },
+    };
+
+    // Stores list of errors to display after validation
+    const errorsList = [];
+
+    // Checks every element from validators
+    Object.keys(validators).forEach((key, i) => {
+      const el = document.getElementById(key);
+      if (!validators[key].reg.test(el.value.trim())) {
+        el.parentNode.classList.add('invalid');
+        errorsList.push(validators[key].msg);
+      } else {
+        el.parentNode.classList.remove('invalid');
+      }
+    });
+
+
+    // Check if card is already expired by comparing current date to expiration date
+    const expDateValue = document.getElementById('exp_date').value;
+    if (validators.exp_date.reg.test(expDateValue)) {
+      const now = new Date();
+      const currentYear = parseInt(now.getFullYear().toString().substring(2), 10);
+      const currentMonth = now.getMonth() + 1;
+      const eneteredMonth = parseInt(validators.exp_date.reg.exec(expDateValue)[1], 10);
+      const enteredYear = parseInt(validators.exp_date.reg.exec(expDateValue)[2], 10);
+
+      if (enteredYear < currentYear || eneteredMonth < currentMonth && enteredYear === currentYear) {
+        errorsList.push('Your card is expired. Make sure you entered valid expiration date.');
+        document.getElementById('exp_date').parentNode.classList.add('invalid');
+      } else {
+        document.getElementById('exp_date').parentNode.classList.remove('invalid');
+      }
+    }
+
+    const errorMessageDiv = document.getElementById('error_message');
+    const successMessageDiv = document.getElementById('success_message');
+    if (!errorsList.length) { // No errors, show success message
+      errorMessageDiv.style.display = 'none';
+      errorMessageDiv.textContent = '';
+      successMessageDiv.style.display = 'block';
+    } else {
+      successMessageDiv.style.display = 'none';
+      errorMessageDiv.style.display = 'block';
+      // Show all errors in a list
+      errorMessageDiv.innerHTML = `<ul>${errorsList.map((error) => `<li>${error}</li>`).join('')}</ul>`;
+    }
+  });
 })();
